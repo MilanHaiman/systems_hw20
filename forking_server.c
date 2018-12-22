6416ac2074c809fda2fc4c2063c2bfe2dce37782
Milan Haiman <mhaiman@stuy.edu>
2018-12-22 16:11:55 -0500

    finished

--- forking_server.c
+++ forking_server.c
@@ -15,22 +15,43 @@ int main() {
 	char processed[BUFFER_SIZE];
 	int r;
 	int i;
+	int f;
 	while(1) {
-	  from_client = server_handshake( &to_client );
-	  while(1) {
-		  r = read(from_client, data, BUFFER_SIZE);
-		  if(r) {
-		  	printf("server received data: \"%s\"\n", data);
-		  	for(i=0;2*i<strlen(data);i++) {
-		  		processed[i]=data[2*i];
-		  	}
-		  	processed[i]=0;
-		  	write(to_client, processed, BUFFER_SIZE);
-		  	printf("server sent processed data: \"%s\"\n", processed);
-			}
-			else {
-				break;
+	  	// start handshake
+		mkfifo("WKP", 0666);
+  		printf("server made WKP\n");
+
+  		from_client = open("WKP", O_RDONLY);
+  		printf("server connected to upstream pipe\n");
+
+  		f = fork();
+		if (f) {
+			close(from_client);
+			remove("WKP");
+		 	printf("server removed WKP\n");
+		}
+		else {
+			// finish handshake
+			subserver_handshake(&to_client, from_client);
+			while(1) {
+			  	r = read(from_client, data, BUFFER_SIZE);
+			  	// printf("subserver %d has r = %d\n", getpid(), r);
+			  	if(r) {
+				  	printf("subserver %d received data: \"%s\"\n", getpid(), data);
+				  	for(i=0;2*i<strlen(data);i++) {
+				  		processed[i]=data[2*i];
+				  	}
+				  	processed[i]=0;
+				  	write(to_client, processed, BUFFER_SIZE);
+				  	printf("subserver %d sent processed data: \"%s\"\n", getpid(), processed);
+				}
+				else {
+					printf("subserver %d exiting\n", getpid());
+					return 0;
+				}
 			}
 		}
+
+	  	
   }
 }
--- pipe_networking.c
+++ pipe_networking.c
@@ -2,37 +2,30 @@
 
 
 /*=========================
-  server_handshake
-  args: int * to_client
+  subserver_handshake
+  args: int * to_client, int from_client
 
-  Performs the client side pipe 3 way handshake.
+  Performs the subserver side pipe 3 way handshake.
   Sets *to_client to the file descriptor to the downstream pipe.
 
   returns the file descriptor for the upstream pipe.
   =========================*/
-int server_handshake(int *to_client) {
-  mkfifo("WKP", 0666);
-  printf("server made WKP\n");
-
-  int upfd = open("WKP", O_RDONLY);
-  printf("server connected to upstream pipe\n");
+int subserver_handshake(int *to_client, int from_client) {
+  int upfd = from_client;
 
   char private_name[HANDSHAKE_BUFFER_SIZE];
   read(upfd, private_name, HANDSHAKE_BUFFER_SIZE);
-  printf("server received message: \"%s\"\n", private_name);
-
-  remove("WKP");
-  printf("server removed WKP\n");
+  printf("subserver %d received message: \"%s\"\n", getpid(), private_name);
 
   int downfd = open(private_name, O_WRONLY);
-  printf("server connected to downstream pipe\n");
+  printf("subserver %d connected to downstream pipe\n", getpid());
 
   write(downfd, private_name, HANDSHAKE_BUFFER_SIZE);
-  printf("server sent message: \"%s\"\n", private_name);
+  printf("subserver %d sent message: \"%s\"\n", getpid(), private_name);
 
   char message[HANDSHAKE_BUFFER_SIZE];
   read(upfd, message, HANDSHAKE_BUFFER_SIZE);
-  printf("server received message: \"%s\"\n", message);
+  printf("subserver %d received message: \"%s\"\n", getpid(), message);
 
   *to_client = downfd;
 
@@ -50,7 +43,8 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-  char private_name[HANDSHAKE_BUFFER_SIZE] = "private";
+  char private_name[HANDSHAKE_BUFFER_SIZE];
+  sprintf(private_name, "-%d-", getpid());
   mkfifo(private_name, 0666);
   printf("client made pipe\n");
 
